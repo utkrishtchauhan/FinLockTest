dab09950bc4947f39de0210a28fc4dad410d2cb9
Utkrisht Chauhan <48377060+utkrishtchauhan@users.noreply.github.com>
2023-05-24 16:05:03 +0530

    Initial Commit

---
+++ FinLockTest/FinLockTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinLockTestApp.swift
+//  FinLockTest
+//
+//  Created by utkrisht chauhan on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FinLockTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
